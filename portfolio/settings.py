52bbf8001776abfa1a38a259fca16fb53b9b2143
pranavratheesh <pranavc493@gmail.com>
2025-12-04 11:52:58 +0530

    host implemented

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -71,20 +71,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # Development: SQLite | Production: PostgreSQL (PythonAnywhere MySQL)
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '3306',
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-        )
-    }
-
+} 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
